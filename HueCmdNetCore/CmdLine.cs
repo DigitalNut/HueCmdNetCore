4acfeb43e9c41f9b98b7bd980c2872875f4ffb85
Digital Nut <DigitalNut@users.noreply.github.com>
2017-11-07 02:55:41 -0800

    HueCmd - .Net Core 2.0 version. Works on Windows desktop and Windows IOT (arm version under Exe Folder).

---
+++ HueCmdNetCore/CmdLine.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HueCmdNetCore
+{
+    partial class Program
+    {
+        static bool ParseCmdLine(string[] args)
+        {
+            int len = args.Length;
+
+            if (len == 0)
+            {
+                PrintUsage();
+                return false;
+            }
+
+            try
+            {
+                int i = 0;
+                while (i < len)
+                {
+                    switch (args[i])
+                    {
+                        case "-ip":
+                            i++;
+                            CmdLineOptions.ip = args[i];
+                            break;
+                        case "-light":
+                            i++;
+                            CmdLineOptions.light = args[i];
+                            break;
+                        case "-brightness":
+                            i++;
+                            CmdLineOptions.brightness = Byte.Parse(args[i]);
+                            break;
+                        case "-color":
+                            i++;
+                            CmdLineOptions.color = args[i];
+                            break;
+                        case "-key":
+                            i++;
+                            CmdLineOptions.key = args[i];
+                            break;
+                        case "-register":
+                            i++;
+                            CmdLineOptions.Register = args[i];
+                            i++;
+                            CmdLineOptions.key = args[i];
+                            break;
+                        case "-status":
+                            CmdLineOptions.status = true;
+                            break;
+                        case "-on":
+                            CmdLineOptions.on = true;
+                            break;
+                        case "-off":
+                            CmdLineOptions.off = true;
+                            break;
+                        default:
+                            PrintUsage();
+                            return false;
+                    }
+                    i++;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid or missing parameters. Please check.");
+                Console.WriteLine("Enter command with no parameters to see help");
+                return false;
+            }
+
+            if (String.IsNullOrEmpty(CmdLineOptions.key))
+            {
+                Console.WriteLine("Missing key. Is you don't have a key, use the register option to assign one");
+                return false;
+            }
+
+            if (!String.IsNullOrEmpty(CmdLineOptions.Register))
+            {
+                if (String.IsNullOrEmpty(CmdLineOptions.key))
+                {
+                    Console.WriteLine("Missing key. Register options requires both app name and key");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Print usage
+        /// </summary>
+        static void PrintUsage()
+        {
+            Console.WriteLine("HueCmdNetCore 1.0");
+            Console.WriteLine(" by DigitalNut");
+            Console.WriteLine(" Controls the Philip Hue from your command line\n");
+            Console.WriteLine("-ip address          - [optional] IP address of HUE, otherwise will try to \n\t\t\tlocate it automatically");
+            Console.WriteLine("-key <key>           - [mandatory] App Key needed to connect to the Hue");
+            Console.WriteLine("-light <id>          - Light to control. Enter 1 to max lights. 0 is only valid with -status");
+            Console.WriteLine("-brightness <number> - Brightness level 0 - 255");
+            Console.WriteLine("-color <color>       - Color value in hex 'rrggbb'. E.g. 00FF00");
+            Console.WriteLine("                       or <color> can also be 'Once' for alert once or 'Multi' for multiple alerts");
+            Console.WriteLine("                       or <color> can also be 'ColorLoop' to start a color loop");
+            Console.WriteLine("                       or <color> can be 'None' to stop ColorLoop or Multi Alert");
+            Console.WriteLine("-on                  - Turns the light off");
+            Console.WriteLine("-off                 - Turns the light on");
+            Console.WriteLine("-status              - Returns the status of the bridge or light. Use the -light option to specify the light.");
+            Console.WriteLine("                       Use 0 for status of all lights");
+            Console.WriteLine("                       This option overrides other commands");
+            Console.WriteLine("-register <appName> <appkey>  \n\t\t\t- Registry App Name & App Key with the Hue. Requires Name and Key. \n" +
+                              "\t\t\tA Key must be registered with the Hue before using the other options \n" +
+                              "\t\t\tExample: HueCmd -register HueCmd SomeKey1234");
+            Console.WriteLine("Examples:");
+            Console.WriteLine("\tHueCmd -key SomeKey1234 -light 4 -brightness 10\t\tSets light 4 to brightness level of 10");
+            Console.WriteLine("\tHueCmd -key SomeKey1234 -light 3 -color ff0000\t\tSets light 3 to red");
+        }
+    }
+}
--- HueCmdNetCore/Program.cs
+++ HueCmdNetCore/Program.cs
@@ -1,12 +1,251 @@
-﻿using System;
+﻿using Q42.HueApi;
+using Q42.HueApi.ColorConverters.HSB;
+using Q42.HueApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HueCmdNetCore
 {
-    class Program
+    partial class Program
     {
+        struct StructCmdLineOptions
+        {
+            public string ip;
+            public Byte? brightness;
+            public string color;
+            public bool? on;
+            public bool? off;
+            public string light;
+            public string key;
+            public string Register;
+            public bool? status;
+        }
+
+        static StructCmdLineOptions CmdLineOptions;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Hack to get the Raspberry PI to connect when running under Mono (on Linux). Otherwise Mono on the Pi doesn't connect 
+            //  to any HTTPS sites that don't have a certificate. This just bypasses the cert test
+            // http://stackoverflow.com/questions/3285489/mono-problems-with-cert-and-mozroots
+            //ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => { return true; };
+
+            if (!ParseCmdLine(args))
+            {
+                Environment.Exit(1);
+                return;
+            }
+
+            try
+            {
+                if (!String.IsNullOrEmpty(CmdLineOptions.Register))
+                {
+                    Task t = Register();
+                    t.Wait();
+                }
+                else if (CmdLineOptions.status != null)
+                {
+                    RunStatusCommand().Wait();
+                    //t.Wait();
+                }
+                else
+                {
+                    Task t = RunLightCommand();
+                    t.Wait();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not connect to Hue.");
+                string err = e.Message;
+                if (e.InnerException != null)
+                    err = e.InnerException.Message;
+                Console.WriteLine("Additional detail: " + err);
+            }
+
+            Environment.Exit(0);
+        }
+
+
+
+        async static Task Register()
+        {
+            string ip = await GetOrFindIP();
+
+            if (String.IsNullOrEmpty(ip))
+                return;
+
+            ILocalHueClient client = new LocalHueClient(ip);
+            var appKey = await client.RegisterAsync(CmdLineOptions.Register, CmdLineOptions.key);
+        }
+
+        async static Task RunLightCommand()
+        {
+            LocalHueClient client = await GetClient();
+
+            if (client == null)
+                return;
+
+            var command = new LightCommand();
+            if (CmdLineOptions.on != null)
+                command.On = true;
+            else if (CmdLineOptions.off != null)
+                command.On = false;
+
+            if (CmdLineOptions.brightness != null)
+                command.Brightness = CmdLineOptions.brightness;
+
+            if (!String.IsNullOrEmpty(CmdLineOptions.color))
+            {
+                if (String.Compare(CmdLineOptions.color, "Once", true) == 0)
+                    command.Alert = Alert.Once;
+                else if (String.Compare(CmdLineOptions.color, "Multi", true) == 0)
+                    command.Alert = Alert.Multiple;
+                else if (String.Compare(CmdLineOptions.color, "ColorLoop", true) == 0)
+                    command.Effect = Effect.ColorLoop;
+                else if (String.Compare(CmdLineOptions.color, "None", true) == 0)
+                {
+                    command.Effect = Effect.None;
+                    command.Alert = Alert.None;
+                }
+                else
+                {
+                    if (CmdLineOptions.color.Length != 6)
+                    {
+                        Console.WriteLine("Color value must be 6 characters. E.g. 00ff00");
+                        return;
+                    }
+
+                    command.SetColor(new Q42.HueApi.ColorConverters.RGBColor(CmdLineOptions.color));
+                }
+            }
+
+            //List<string> lights = new List<string>();
+            //lights.Add(CmdLineOptions.light);
+            await client.SendCommandAsync(command, new List<string> { CmdLineOptions.light });
+        }
+
+
+        static async Task RunStatusCommand()
+        {
+            LocalHueClient client = await GetClient();
+
+            if (client == null)
+                return;
+
+            if (String.IsNullOrEmpty(CmdLineOptions.light))
+            {
+                var config = await client.GetBridgeAsync();
+                if (config != null)
+                {
+                    Console.WriteLine("        Model: " + config.Config.ModelId);
+                    Console.WriteLine("         Name: " + config.Config.Name);
+                    Console.WriteLine("   SW Version: " + config.Config.SoftwareVersion);
+                    Console.WriteLine("  API Version: " + config.Config.ApiVersion);
+                    Console.WriteLine("  Portal Conn: " + config.Config.PortalConnection);
+                }
+            }
+            else if (String.Compare(CmdLineOptions.light, "0") == 0)
+            {
+                var lights = await client.GetLightsAsync();
+                if (lights == null)
+                {
+                    Console.WriteLine("Could not enumerate list. Hue may not be reachable.");
+                    return;
+                }
+                foreach (Light light in lights)
+                {
+                    PrintLightDetail(light);
+                    Console.WriteLine("");
+                }
+            }
+            else
+            {
+                var light = await client.GetLightAsync(CmdLineOptions.light);
+                if (light == null)
+                {
+                    Console.WriteLine("Light ID not found");
+                    return;
+                }
+
+                PrintLightDetail(light);
+            }
+        }
+
+        static void PrintLightDetail(Light light)
+        {
+            Console.WriteLine("         ID: " + light.Id);
+            Console.WriteLine("       Name: " + light.Name);
+            Console.WriteLine("State ----------------------------------------- ");
+            Console.WriteLine("  Reachable: " + light.State.IsReachable);
+            Console.WriteLine("         On: " + light.State.On);
+            Console.WriteLine(" Brightness: " + light.State.Brightness);
+            Console.WriteLine("     Effect: " + light.State.Effect);
+            Console.WriteLine("      Alert: " + light.State.Alert);
+            Console.WriteLine("Info ------------------------------------------ ");
+            Console.WriteLine("       Type: " + light.Type);
+            Console.WriteLine("   Model ID: " + light.ModelId);
+            Console.WriteLine("Manufacture: " + light.ManufacturerName);
+            Console.WriteLine("  Unique ID: " + light.UniqueId);
+            Console.WriteLine(" SW Version: " + light.SoftwareVersion);
+        }
+
+        /// <summary>
+        /// Helper function to create a HueCient
+        /// </summary>
+        /// <returns></returns>
+        static async Task<LocalHueClient> GetClient()
+        {
+            LocalHueClient client = null;
+
+            string ip = await GetOrFindIP();
+
+            if (String.IsNullOrEmpty(ip))
+                return null;
+
+            client = new LocalHueClient(ip);
+            client.Initialize(CmdLineOptions.key);
+
+            if (!client.IsInitialized)
+                return null;
+
+            return client;
+        }
+
+        /// <summary>
+        /// Return the command line IP address that was entered by the user or IP found by the bridge locater service
+        /// </summary>
+        /// <param name="ip"></param>
+        static async Task<string> GetOrFindIP()
+        {
+            string ip = CmdLineOptions.ip;
+
+            if (String.IsNullOrEmpty(CmdLineOptions.ip))
+            {
+                IBridgeLocator locator = new HttpBridgeLocator();
+                IEnumerable<Q42.HueApi.Models.Bridge.LocatedBridge> bridgeIPs = await locator.LocateBridgesAsync(TimeSpan.FromSeconds(10));
+
+                ////For Windows 8 and .NET45 projects you can use the SSDPBridgeLocator which actually scans your network. 
+                ////See the included BridgeDiscoveryTests and the specific .NET and .WinRT projects
+                //SSDPBridgeLocator locator = new SSDPBridgeLocator();
+                //IEnumerable<string> bridgeIPs = await locator.LocateBridgesAsync(TimeSpan.FromSeconds(10));
+
+                if (bridgeIPs.Any())
+                {
+                    ip = bridgeIPs.First().IpAddress;
+                    Console.WriteLine("Bridge found using IP address: " + ip);
+                }
+                else
+                {
+                    Console.WriteLine("Scan did not find a Hue Bridge. Try suppling a IP address for the bridge");
+                    return null;
+                }
+            }
+
+            return ip;
         }
     }
 }
+
